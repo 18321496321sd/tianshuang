4158883e76cb81e1a7a48caf9a13d13fab0da3a9
18321496321sd <112454977+18321496321sd@users.noreply.github.com>
2023-05-07 17:03:03 +0800

    Update WechatConfig.java

--- src/main/java/com/pt/vx/config/WechatConfig.java
+++ src/main/java/com/pt/vx/config/WechatConfig.java
@@ -43,7 +43,7 @@ public class WechatConfig {
                 "田爽", //咋称呼这个人
                 "宁夏回族自治银川市西夏区", //这个人的详细地址
                 "银川", //这个人在的城市
-                new BirthDay(2000,10,21,true,false,"pt生日快乐！！"),
+                new BirthDay(2000,10,21,false,false,"pt生日快乐！！"),
                 new BirthDay(2002,8,25,false,false,"生日快乐哦~~"),
                 new BirthDay(2024,1,7,true,true),
                 new BirthDay(2023,1,7,true,false,"周年快乐！！！")
@@ -57,7 +57,7 @@ public class WechatConfig {
                 "吴江",
                 new BirthDay(2002,8,25,false,false,"生日快乐哦~~"),
                 new BirthDay(2000,10,21,false,false,"pt生日快乐！！"),
-                new BirthDay(2023,1,7,true,true),
+                new BirthDay(2024,1,7,true,true),
                 new BirthDay(2023,1,7,true,false,"周年快乐！！！")
         ));
 
